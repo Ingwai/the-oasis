ed162607578755b552e9eb9ea71c892ec627e7d3
Ingwai <ingwai@wp.pl>
2023-08-27 13:14:51 +0200

    implamantation supabase api for backend and create basic DB

--- src/App.jsx
+++ src/App.jsx
@@ -36,3 +36,19 @@ export default function App() {
 }
 
 // 	<GlobalStyles /> nie może mieć children jedynie może być rodzeństem sibling
+
+// npm i --save @supabase/supabase-js
+
+// orginalny link do bazy w supabase i RLS
+/*
+curl 'https://ltmpstvcevfcoahibizw.supabase.co/rest/v1/cabins?select=*' \ -H "apikey: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx0bXBzdHZjZXZmY29haGliaXp3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTMwNzY5NDMsImV4cCI6MjAwODY1Mjk0M30.EGgBFJiA63FBMKNO8-MWOVynNg05rgES5s18K7Jh55I" \ -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx0bXBzdHZjZXZmY29haGliaXp3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTMwNzY5NDMsImV4cCI6MjAwODY1Mjk0M30.EGgBFJiA63FBMKNO8-MWOVynNg05rgES5s18K7Jh55I"
+
+*/
+
+// ZMIANY
+//1. trzeba wkleić w command prompt: zmienić pojedyncze cudzysłowy w https na podwójne
+
+/*
+curl "https://ltmpstvcevfcoahibizw.supabase.co/rest/v1/cabins?select=*" \ -H "apikey: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx0bXBzdHZjZXZmY29haGliaXp3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTMwNzY5NDMsImV4cCI6MjAwODY1Mjk0M30.EGgBFJiA63FBMKNO8-MWOVynNg05rgES5s18K7Jh55I" \ -H  "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx0bXBzdHZjZXZmY29haGliaXp3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTMwNzY5NDMsImV4cCI6MjAwODY1Mjk0M30.EGgBFJiA63FBMKNO8-MWOVynNg05rgES5s18K7Jh55I"
+
+*/
--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,13 +1,27 @@
-import Heading from "../ui/Heading";
-import Row from "../ui/Row";
+import { useEffect } from 'react';
+import { getCabins } from '../services/apiCabins';
+import Heading from '../ui/Heading';
+import Row from '../ui/Row';
 
 function Cabins() {
-  return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-    </Row>
-  );
+	useEffect(() => {
+		getCabins().then(data => console.log(data));
+
+		// async - await
+		// const getCabinsData = async () => {
+		// 	const data = await getCabins();
+		// 	console.log(data);
+		// };
+		// getCabinsData();
+	}, []);
+
+	return (
+		<Row type='horizontal'>
+			<Heading as='h1'>All cabins</Heading>
+			<p>TEST</p>
+			<img src='https://ltmpstvcevfcoahibizw.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg' />
+		</Row>
+	);
 }
 
 export default Cabins;
